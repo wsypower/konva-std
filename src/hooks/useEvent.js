a9cbab9850982f9f395f594c9b43d369d341c8f5
weiyafei <2553241022@qq.com>
2022-08-15 11:23:15 +0800

    feat(custom): 修改event
    添加事件hooks

--- src/hooks/useEvent.js
+++ src/hooks/useEvent.js
@@ -2,16 +2,12 @@
  * @Description:
  * @Author: wsy
  * @Date: 2022-08-15 10:59:02
- * @LastEditTime: 2022-08-15 10:59:21
+ * @LastEditTime: 2022-08-15 11:22:39
  * @LastEditors: wsy
  */
-export function useUpdateTitle() {
-  const route = useRoute()
-  const title = ref(route.meta.title)
-  onBeforeRouteUpdate(changeTitle)
-  onBeforeRouteLeave(changeTitle)
-  function changeTitle(to) {
-    title.value = to.meta.title
-  }
-  return title
+import mitt from 'mitt'
+
+const emitter = mitt()
+export function useEmitter() {
+  return emitter
 }
