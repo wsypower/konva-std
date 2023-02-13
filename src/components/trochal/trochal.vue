492bd36adebf13bc63f05065f5cc8d51cf049e9e
weiyafei <2553241022@qq.com>
2023-02-13 15:26:19 +0800

    feat(custom): 轮盘组件初始化

---
+++ src/components/trochal/trochal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-dark-200" :style="{ width: props.width, height: props.height }">
+    <canvas></canvas>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    width: {
+      type: String,
+      default() {
+        return '100%'
+      }
+    },
+    height: {
+      type: String,
+      default() {
+        return '100%'
+      }
+    },
+    data: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  })
+</script>
+
+<style lang="scss" scoped></style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,26 +2,14 @@
  * @Description:
  * @Author: wsy
  * @Date: 2022-07-11 15:30:05
- * @LastEditTime: 2022-08-02 16:25:52
+ * @LastEditTime: 2023-02-13 15:18:04
  * @LastEditors: wsy
 -->
 <template>
-  <div flex="~ col" w="full" h="full" bg="red-500">
-    <header w="full" flex="none" h="68px">
-      <LayoutHeader />
-    </header>
-    <main w="full" flex="auto" h="0px">
-      <LayoutContainer />
-    </main>
+  <div flex="~" w="full" h="full" bg="#01091D">
+    <div w="50%" h="100%"></div>
+    <trochal width="50%"></trochal>
   </div>
 </template>
 
-<script setup>
-  import LayoutHeader from './components/LayoutHeader.vue'
-  import LayoutContainer from './components/LayoutContainer.vue'
-
-  const { API_DEMO_POST } = useRequest()
-  API_DEMO_POST().then((res) => {
-    console.info(res)
-  })
-</script>
+<script setup></script>
