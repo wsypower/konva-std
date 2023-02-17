7d7bcc120dd0f0f73303097fc7805f0365119976
weiyafei <2553241022@qq.com>
2023-02-17 21:39:16 +0800

    feat(custom): 视屏放入视图

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,15 +2,17 @@
  * @Description:
  * @Author: wsy
  * @Date: 2022-07-11 15:30:05
- * @LastEditTime: 2023-02-16 18:28:44
+ * @LastEditTime: 2023-02-17 20:22:58
  * @LastEditors: wsy
 -->
 <template>
-  <div flex="~" w="full" h="full" bg="#03162D">
-    <trochal></trochal>
+  <div w="full" h="full" bg="#03162D" class="relative">
+    <video :src="video" autoplay muted loop class="absolute"></video>
+    <trochal class="absolute"></trochal>
   </div>
 </template>
 
 <script setup>
+  import video from './AI-60fps.mp4'
   import trochal from '@/components/trochal/useTrochal.js'
 </script>
